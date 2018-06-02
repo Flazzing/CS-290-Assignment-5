b15667490bfb3fdaf488b0e2dda6a41198680c82
Lim Cheng Qing <limchengqing@yahoo.com>
2018-06-02 01:38:00 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,22 +2,48 @@
  * Write your routing code in this file.  Make sure to add your name and
  * @oregonstate.edu email address below.
  *
- * Name:
- * Email:
+ * Name: Lim Cheng Qing
+ * Email: limche@oregonstate.edu
  */
 
-var path = require('path');
-var express = require('express');
+	var path = require('path');
+	var express = require('express');
+	var exphbs = require('express-handlebars');
+	var fs = require('fs');
+	var app = express();
 
-var app = express();
-var port = process.env.PORT || 3000;
+	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+	app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
+	var port = process.env.PORT || 3000;
 
-app.get('*', function (req, res) {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
+	var rawData = fs.readFileSync('./twitData.json');
+	var twitData = JSON.parse(rawData); //get the tweet data (text/authors)
 
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
-});
+
+	app.get('/', function (req, res) {
+	  res.status(200).render('newView', {data: twitData, index: 1}); //index: should you render create twit button 
+	});
+
+	app.get('/twits/%3C:twit%3E', function (req, res){
+    	var twitId = req.params.twit;
+		console.log(twitId);
+		var singleTwitArray = [];
+		if((twitId >= twitData.length) || (twitId < 0)){ 
+			res.status(404).render('error');
+		}
+		else{ 
+			singleTwitArray.push(twitData[twitId]); 
+			res.status(200).render('newView', {data: singleTwitArray, index: 0}); 
+		}
+	});
+
+	app.use(express.static('public'));
+
+	app.get('*', function (req, res) {
+	  res.status(404).render('error');
+	});
+
+	app.listen(port, function () {
+	  console.log("== Server is listening on port", port);
+	});
